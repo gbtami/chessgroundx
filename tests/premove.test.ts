7cff16980517a72e402b35deed84fd90470e9a4d
Ada Joule <ada.fulmina@gmail.com>
2022-07-19 00:44:23 +0700

    Incorporate the changes in the test cases

--- tests/premove.test.ts
+++ tests/premove.test.ts
@@ -1,4 +1,3 @@
-import { Geometry } from '../src/types';
 import { configure } from '../src/config';
 import { State, defaults } from '../src/state';
 import { premove } from '../src/premove';
@@ -7,12 +6,12 @@ import { expect } from 'chai';
 describe('premove() test', () => {
   it('chess white king', () => {
     const state = defaults() as State;
-    configure(state, { geometry: Geometry.dim8x8, variant: 'chess', fen: '8/8/8/8/8/8/8/R3K2R w QK' });
+    configure(state, { dimensions: { width: 8, height: 8 }, variant: 'chess', fen: '8/8/8/8/8/8/8/R3K2R w QK' });
 
-    console.log(state.variant, state.pieces);
+    console.log(state.variant, state.boardState);
     const expected = ['a1', 'c1', 'd1', 'd2', 'e2', 'f2', 'f1', 'g1', 'h1'];
     const premoves = premove(
-      state.pieces,
+      state.boardState.pieces,
       'e1',
       state.premovable.castle,
       state.dimensions,
@@ -26,12 +25,12 @@ describe('premove() test', () => {
 describe('premove() test', () => {
   it('janggi white king', () => {
     const state = defaults() as State;
-    configure(state, { geometry: Geometry.dim9x10, variant: 'janggi', fen: '9/9/9/9/9/9/9/9/4K4/9' });
+    configure(state, { dimensions: { width: 9, height: 10 }, variant: 'janggi', fen: '9/9/9/9/9/9/9/9/4K4/9' });
 
-    console.log(state.variant, state.pieces);
+    console.log(state.variant, state.boardState);
     const expected = ['d1', 'd2', 'd3', 'e1', 'e3', 'f1', 'f2', 'f3'];
     const premoves = premove(
-      state.pieces,
+      state.boardState.pieces,
       'e2',
       state.premovable.castle,
       state.dimensions,
@@ -45,12 +44,12 @@ describe('premove() test', () => {
 describe('premove() test', () => {
   it('janggi black pawn', () => {
     const state = defaults() as State;
-    configure(state, { geometry: Geometry.dim9x10, variant: 'janggi', fen: '9/9/9/9/9/9/9/9/4p4/9' });
+    configure(state, { dimensions: { width: 9, height: 10 }, variant: 'janggi', fen: '9/9/9/9/9/9/9/9/4p4/9' });
 
-    console.log(state.variant, state.pieces);
+    console.log(state.variant, state.boardState);
     const expected = ['d1', 'd2', 'e1', 'f1', 'f2'];
     const premoves = premove(
-      state.pieces,
+      state.boardState.pieces,
       'e2',
       state.premovable.castle,
       state.dimensions,
@@ -64,9 +63,9 @@ describe('premove() test', () => {
 describe('premove() test', () => {
   it('janggi black rook', () => {
     const state = defaults() as State;
-    configure(state, { geometry: Geometry.dim9x10, variant: 'janggi', fen: '9/9/9/9/9/9/9/5r3/9/9' });
+    configure(state, { dimensions: { width: 9, height: 10 }, variant: 'janggi', fen: '9/9/9/9/9/9/9/5r3/9/9' });
 
-    console.log(state.variant, state.pieces);
+    console.log(state.variant, state.boardState);
     const expected = [
       'a3',
       'b3',
@@ -89,7 +88,7 @@ describe('premove() test', () => {
       'i3',
     ];
     const premoves = premove(
-      state.pieces,
+      state.boardState.pieces,
       'f3',
       state.premovable.castle,
       state.dimensions,
